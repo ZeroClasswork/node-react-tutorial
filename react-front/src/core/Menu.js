28cead7d32218538463be1ac91d61f058537c100
atleastzero <megan.obryan52@gmail.com>
2020-05-17 03:06:00 -0700

    Styling Menu.

--- react-front/src/core/Menu.js
+++ react-front/src/core/Menu.js
@@ -1,12 +1,79 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const Menu = () => (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-    </div>
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return {
+            backgroundColor: "#000000",
+            color: "#dddddd",
+            border: 0,
+            flex: 1
+        }
+    }
+    else {
+        return {
+            backgroundColor: "#222222",
+            color: "#dddddd",
+            border: 0,
+            flex: 1
+        }
+    }
+}
+
+const Menu = ({history}) => (
+    <ul 
+        className="nav nav-tabs" 
+        style={{
+            backgroundColor: "#222222",
+            display: "flex"
+        }}
+    >
+        <li 
+            className="nav-item"
+            style={{
+                display: "flex",
+                flex: 1
+            }}
+        >
+            <Link 
+                style={isActive(history, "/")}
+                className="nav-link" 
+                to="/"
+            >
+                Home
+            </Link>
+        </li>
+        <li 
+            className="nav-item"
+            style={{
+                display: "flex",
+                flex: 1
+            }}
+        >
+            <Link 
+                style={isActive(history, "/signin")}
+                className="nav-link" 
+                to="/signin"
+            >
+                Sign In
+            </Link>
+        </li>
+        <li 
+            className="nav-item"
+            style={{
+                display: "flex",
+                flex: 1
+            }}
+        >
+            <Link 
+                style={isActive(history, "/signup")}
+                className="nav-link" 
+                to="/signup"
+            >
+                Sign Up
+            </Link>
+        </li>
+    </ul>
 )
 
-export default Menu
+export default withRouter(Menu)
